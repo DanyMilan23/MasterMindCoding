dde58c24591764179c7abfabecfc4f252341fc4a
dany_milan <esteban239222@gmail.com>
2020-05-09 22:46:51 -0400

    Material UI y preconfiguraciones

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import React,{ useEffect} from "react";
+import App from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/config/theme";
+
+
+const MyApp = props => {
+  
+  const { Component, pageProps } = props;
+  useEffect(() => { 
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  })
+  return (      
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  ) 
+}
+export default MyApp;
---
+++ src/config/theme.js
@@ -0,0 +1,22 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#33eaff',
+    },
+    secondary: {
+      main: '#ff1744',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default theme;
