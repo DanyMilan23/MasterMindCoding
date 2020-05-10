4fd517172f9092383df42d57a1191ed095eb8fc0
dany_milan <esteban239222@gmail.com>
2020-05-10 13:56:10 -0400

    Correcciones Carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import { Carousel } from "react-responsive-carousel";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
 //import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-export default () => (
-  <Carousel showThumbs={false} infiniteLoop={true} autoPlay>
-    <div>
-      <img alt="" src="/mobile_text.jpg" />
-    </div>
-    <div>
-      <img alt="" src="qa_text.jpg" /> 
-    </div>
-    <div>
-      <img alt="" src="support_text.jpg" />
-    </div>
-    <div>
-      <img alt="" src="web_text.jpg" />
-    </div>
-  </Carousel>
-);
+export default () => {
+  const [source, setSource] = useState(false);
+  const [tam, setTam] = useState(400)
+  //MediaQuery
+  const matches = useMediaQuery("(min-width:960px)");
+  useEffect(() => {
+    setSource(matches);
+  }, [matches]);
+  useEffect(() => {
+    if(source === true){
+      setTam(400);
+    }
+    else{
+      setTam(200);
+    }
+  }, [source]);
+  useEffect(()=>{setTam(400)})
+
+  return(
+    <Carousel showThumbs={false} infiniteLoop={true} showStatus={false} interval={20000} dynamicHeight={true} autoPlay>
+      <div>
+        <img alt="" src="/mobile_text.jpg"  style={{height:500}}/>
+      </div>
+      <div>
+        <img alt="" src="qa_text.jpg" style={{ height: 500}}/> 
+      </div>
+      <div>
+        <img alt="" src="support_text.jpg" style={{ height:500}}/>
+      </div>
+      <div>
+        <img alt="" src="web_text.jpg" style={{ height:500}}/>  
+      </div>
+    </Carousel>
+  )
+};
