224b363e88d6efc2e0bf5abaabb6225b5a4255ad
dany_milan <esteban239222@gmail.com>
2020-05-22 14:41:08 -0400

    Constantes separadas

--- pages/index.js
+++ pages/index.js
@@ -113,6 +113,48 @@ export default function Home() {
   const titleDescriptionIndustry='Telecom'
   const text1Industry='The telecom industry involves communications companies and providers of mobile devices, mobile applications as well as Internet and video streaming services.'
   const text2Industry='Our team of software developers has taken on a range of telecommunication software projects with great success. We are an expert team ready for any telecom software project. '
+  //Technology
+  const titleTechnology='SOLUTIONS'
+  const titleDescriptionTechnology='Custom Software Development'
+  const text1Technology=(<b>
+                  We provide full cycle product development, including design,
+                  implementation, QA and production deployment.
+                  <br />
+                  Our partners trust us to build custom Tech solutions for:{" "}
+                </b>)
+  const text2Technology=( <div>• Architectural Design. <br />
+                • Software Product Development. <br />
+                • Product Design Sprint. <br />
+                • Quality assurance on development. <br />
+                • Test automation during and post development phase. <br />
+                • Software support and maintenance.
+                <br /></div>)
+  //Careers
+  const titleCareers='INDUSTRY'
+  const titleDescriptionCareers='Telecom'
+  const text1Careers='The telecom industry involves communications companies and providers of mobile devices, mobile applications as well as Internet and video streaming services.'
+  const text2Careers='Our team of software developers has taken on a range of telecommunication software projects with great success. We are an expert team ready for any telecom software project. '
+  //About
+  const titleAbout='SOLUTIONS'
+  const titleDescriptionAbout='Custom Software Development'
+  const text1About=(<b>
+                  We provide full cycle product development, including design,
+                  implementation, QA and production deployment.
+                  <br />
+                  Our partners trust us to build custom Tech solutions for:{" "}
+                </b>)
+  const text2About=( <div>• Architectural Design. <br />
+                • Software Product Development. <br />
+                • Product Design Sprint. <br />
+                • Quality assurance on development. <br />
+                • Test automation during and post development phase. <br />
+                • Software support and maintenance.
+                <br /></div>)
+  //Contact
+  const titleContact='INDUSTRY'
+  const titleDescriptionContact='Telecom'
+  const text1Contact='The telecom industry involves communications companies and providers of mobile devices, mobile applications as well as Internet and video streaming services.'
+  const text2Contact='Our team of software developers has taken on a range of telecommunication software projects with great success. We are an expert team ready for any telecom software project. '
 
   return (
     <>
@@ -131,28 +173,28 @@ export default function Home() {
           text2={text2Industry}
           />):null}
           {menu.tech?(<Tipo2
-          title={titleIndustry}
-          titleDescription={titleDescriptionIndustry}
-          text1={text1Industry}
-          text2={text2Industry}
+          title={titleTechnology}
+          titleDescription={titleDescriptionTechnology}
+          text1={text1Technology}
+          text2={text2Technology}
           />):null}
           {menu.careers?(<Tipo1
-          title={titleSolutions}
-          titleDescription={titleDescriptionSolutions}
-          text1={text1Solutions}
-          text2={text2Solutions}
+          title={titleCareers}
+          titleDescription={titleDescriptionCareers}
+          text1={text1Careers}
+          text2={text2Careers}
           />):null}
         {menu.about?(<Tipo2
-          title={titleSolutions}
-          titleDescription={titleDescriptionSolutions}
-          text1={text1Solutions}
-          text2={text2Solutions}
+          title={titleAbout}
+          titleDescription={titleDescriptionAbout}
+          text1={text1About}
+          text2={text2About}
           />):null}
            {menu.contact?(<Tipo1
-          title={titleIndustry}
-          titleDescription={titleDescriptionIndustry}
-          text1={text1Industry}
-          text2={text2Industry}
+          title={titleContact}
+          titleDescription={titleDescriptionContact}
+          text1={text1Contact}
+          text2={text2Contact}
           />):null}
       </Layout>
      
